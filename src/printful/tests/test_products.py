adf649e4dd71b054f25aefb357c52fa805de3662
Christopher Grebs <cg@webshox.org>
2015-01-04 15:51:09 +0100

    More example tests

--- src/printful/tests/test_products.py
+++ src/printful/tests/test_products.py
@@ -20,3 +20,42 @@ def test_products():
 
     assert response.status_code == 200
     assert len(response.json()['result']) == 80
+
+
+@httpretty.activate
+def test_products_variants():
+    httpretty.register_uri(
+        httpretty.GET,
+        'https://api.theprintful.com/products/1',
+        body=get_fixture('products_1.json'),
+        status=200,
+        content_type='application/json',
+    )
+
+    response = Client('').get('products/1')
+
+    assert httpretty.last_request().path == '/products/1'
+
+    assert response.status_code == 200
+    assert response.json()['result']['product']['id'] == 1
+    assert response.json()['result']['product']['type'] == 'POSTER'
+    assert len(response.json()['result']['variants']) == 9
+
+
+@httpretty.activate
+def test_products_variant_detail():
+    httpretty.register_uri(
+        httpretty.GET,
+        'https://api.theprintful.com/products/variant/4464',
+        body=get_fixture('products_variant_4464.json'),
+        status=200,
+        content_type='application/json',
+    )
+
+    response = Client('').get('products/variant/4464')
+
+    assert httpretty.last_request().path == '/products/variant/4464'
+
+    assert response.status_code == 200
+    assert response.json()['result']['variant']['id'] == 4464
+    assert response.json()['result']['product']['id'] == 1
